2fb28c3198f6e797aec258e1e45580b5bc25046c
Johan Durish <jdurish2@gmail.com>
2025-03-14 14:14:31 -0400

    Add selected attribute to selected projects

--- src/ProjectsRenderer.js
+++ src/ProjectsRenderer.js
@@ -3,15 +3,46 @@ import removeChildren from "./removeChildren";
 class ProjectsRenderer {
    constructor(projectsElement) {
       this.projectsElement = projectsElement;
+
+      this.selectedProject = null;
+      this.selectedProjectButton = null;
+
+      this.selectedAttribute = "data-selected";
+   }
+
+   selectProject(project) {
+      this.selectedProject = project;
+      this.selectedProject.render();
+   }
+
+   selectProjectButton(projectButton) {
+      this.selectedProjectButton = projectButton;
+      this.selectedProjectButton.toggleAttribute(this.selectedAttribute);
    }
 
    render(projects) {
       removeChildren(this.projectsElement);
 
+      if (!this.selectedProject) {
+         this.selectProject(projects[0]);
+      }
+
       for (const project of projects) {
          const projectButton = document.createElement("button");
          projectButton.innerText = project.title;
-         projectButton.addEventListener("click", () => project.render());
+
+         if (this.selectedProject === project) {
+            this.selectProjectButton(projectButton);
+         }
+
+         projectButton.addEventListener("click", () => {
+            if (this.selectedProjectButton) {
+               this.selectedProjectButton.removeAttribute(this.selectedAttribute);
+            }
+
+            this.selectProject(project);
+            this.selectProjectButton(projectButton);
+         });
 
          this.projectsElement.appendChild(projectButton);
       }

70c36b9e4ddba315e8498b148a5e1139deb42148
Johan Durish <jdurish2@gmail.com>
2025-03-15 19:34:36 -0400

    Implement complete task button and add add task card

--- src/TasksRenderer.js
+++ src/TasksRenderer.js
@@ -1,19 +1,56 @@
-import removeChildren from "./removeChildren";
 
 class TasksRenderer {
    constructor(tasksElement) {
       this.tasksElement = tasksElement;
       this.addTaskClass = "add-task";
+      this.completeTaskClass = "complete";
+
+      this.project = null;
+      this.addTaskCard = null;
    }
 
    render(project) {
-      removeChildren(this.tasksElement);
+      while (this.tasksElement.firstChild !== this.addTaskCard) {
+         this.tasksElement.removeChild(this.tasksElement.firstChild);
+      }
+      
+      if (this.project !== project) {
+         if (this.addTaskCard) {
+            this.tasksElement.removeChild(this.addTaskCard);
+         }
 
+         this.project = project;
+         this.addTaskCard = document.createElement("li");
+         
+         this.addTaskCard.classList.add(this.addTaskClass);
+
+         const addTaskButton = document.createElement("button");
+
+         this.addTaskCard.appendChild(addTaskButton);
+
+         addTaskButton.innerText = "+\nAdd Task";
+
+         this.tasksElement.appendChild(this.addTaskCard);
+      }
+      
       for (const task of project.tasks) {
          const taskCard = document.createElement("li");
-         taskCard.innerText = JSON.stringify(task);
-
-         this.tasksElement.appendChild(taskCard);
+         const taskTitle = document.createElement("h2");
+         const completeTaskButton = document.createElement("button");
+         
+         taskCard.appendChild(taskTitle);
+         taskCard.appendChild(completeTaskButton);
+         
+         taskTitle.innerText = task.title;
+         
+         completeTaskButton.innerText = "✓";
+         completeTaskButton.classList.add(this.completeTaskClass);
+         
+         completeTaskButton.addEventListener("click", () => {
+            project.removeTask(task);
+         });
+         
+         this.tasksElement.insertBefore(taskCard, this.addTaskCard);
       }
    }
 }
